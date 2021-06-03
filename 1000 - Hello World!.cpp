bf8ca7e88481cc2bfe1f494f28d0bf81499d7a76
kille <killeandrew@gmail.com>
2021-06-03 12:13:44 -0400

    update

---
+++ 1000 - Hello World!.cpp
@@ -0,0 +1,15 @@
+/*
+    author: kille
+*/
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+ 
+using namespace std;
+ 
+ int main()
+ {
+     cout << "Hello World!" << endl;
+     return 0;
+ }

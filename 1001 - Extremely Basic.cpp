dfe0328d8b9768e0aa7ec7c7079aacab9d302c3f
kille <killeandrew@gmail.com>
2021-06-03 12:58:17 -0400

    update

---
+++ 1001 - Extremely Basic.cpp
@@ -0,0 +1,18 @@
+/*
+    author: kille
+*/
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+ 
+using namespace std;
+ 
+ int main()
+ {
+     int a, b, x;
+     cin >> a >> b;
+     x = a + b;
+     cout << "X = " + x << endl;
+     return 0;
+ }
